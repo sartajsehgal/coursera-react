db5e7073c0cecb048bc347a730c3c19abd40a566
sartajsehgal <sartajsehgal16@gmail.com>
2020-07-30 00:24:41 +0530

    redux thunk

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -6,6 +6,7 @@ import {
   } from 'reactstrap';
   import {Link} from 'react-router-dom';
 import { LocalForm,Control,Errors } from 'react-redux-form';
+import { Loading } from './LoadingComponent';
 
 function RenderDish({dish}) {
     return(
@@ -125,6 +126,24 @@ class CommentForm extends Component {
 }
             
         const  DishDetail = (props) => {
+            if(props.isLoading) {
+                return(
+                    <div className="container">
+                        <div className="row">
+                            <Loading />
+                        </div>
+                    </div>
+                );
+            }
+            else if(props.errMess) {
+                return(
+                    <div className="container">
+                        <div className="row">
+                            <h4>{props.errMess}</h4>
+                        </div>
+                    </div>
+                );
+            }
                 if(props.dish != null)
                 return (
                     <div className="container">
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -9,8 +9,8 @@ import Footer from './FooterComponent';
 import About from './AboutComponent';
 import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
-import { addComment } from '../redux/ActionCreators';
-
+import { addComment,fetchDishes } from '../redux/ActionCreators';
+import {actions} from 'react-redux-form';
 
 const mapStateToProps = state => {
     return {
@@ -22,21 +22,29 @@ const mapStateToProps = state => {
 }
 
 const mapDispatchToProps = (dispatch) => ({
-    addComment: (dishId,rating,author,comment) => dispatch(addComment(dishId,rating,author,comment))
+    addComment: (dishId,rating,author,comment) => dispatch(addComment(dishId,rating,author,comment)),
+    fetchDishes: () => {dispatch(fetchDishes())},
+    resetFeedbackFrom: () => {dispatch(actions.reset('feedback'))}
 });
 
 class Main extends Component {
 
   constructor(props) {
      super(props);
+
   }
 
+  componentDidMount() {
+    this.props.fetchDishes(); 
+  }
 
 render() {
 
   const HomePage = () => {
     return(
-      <Home dish={this.props.dishes.filter((dish) => dish.featured)[0]}
+      <Home dish={this.props.dishes.dishes.filter((dish) => dish.featured)[0]}
+      dishesLoading = {this.props.dishes.isLoading}
+      dishesErrMess = {this.props.dishes.errMess}
       promotion={this.props.promotions.filter((promo) => promo.featured)[0]} 
       leader={this.props.leaders.filter((leader) => leader.featured)[0]}
       />
@@ -45,9 +53,11 @@ render() {
 
 const DishWithId = ({match}) => {
   return(
-    <DishDetail dish={this.props.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]}
-    comments={this.props.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))} 
-    addComment={this.props.addComment}
+    <DishDetail dish={this.props.dishes.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]}
+      isLoading = {this.props.dishes.isLoading}
+      errMess = {this.props.dishes.errMess}
+      comments={this.props.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))} 
+      addComment={this.props.addComment}
     />
   );
 }
@@ -59,7 +69,7 @@ const DishWithId = ({match}) => {
         <Route path="/home" component={HomePage} />
         <Route exact path="/menu" component={() => <Menu dishes={this.props.dishes} />} />
         <Route path="/menu/:dishId" component={DishWithId} />
-        <Route exact path="/contactus" component={Contact} />
+        <Route exact path="/contactus" component={() => <Contact resetFeedbackForm={this.props.resetFeedbackForm} />}/>
         <Route exact path="/aboutus" component={() => <About leaders={this.props.leaders} />} />
         <Redirect to="/home" />
       </Switch>
